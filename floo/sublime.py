446cc998d134865b778d9333dae097bd935bfa79
Geoff Greer <geoff@greer.fm>
2013-03-21 22:49:41 -0700

    Make a silly sublime stub file for now

---
+++ floo/sublime.py
@@ -0,0 +1,20 @@
+
+
+def windows(*args, **kwargs):
+    pass
+
+
+def set_timeout(*args, **kwargs):
+    pass
+
+
+def load_settings(*args, **kwargs):
+    pass
+
+
+def error_message(*args, **kwargs):
+    pass
+
+
+class Region():
+    pass
