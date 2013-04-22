1a7da6dbaf9060fcb017e691ca8fd8826230021a
Geoff Greer <geoff@greer.fm>
2013-04-22 12:33:08 -0700

    Damn. typo

--- plugin/floo/utils.py
+++ plugin/floo/utils.py
@@ -84,7 +84,7 @@ def load_settings():
     settings = load_floorc()
     if not settings:
         msg.error('you should probably define some stuff in your ~/.floorc file')
-    G.COLAB_DIR = os.path.expanduser(settings.get('share_dir', '~/.floobits/shared/'))
+    G.COLAB_DIR = os.path.expanduser(settings.get('share_dir', '~/.floobits/share/'))
     mkdir(G.COLAB_DIR)
     for name, val in settings.items():
         setattr(G, name, val)
