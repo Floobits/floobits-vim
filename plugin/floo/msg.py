0e8433972b4675d838146176b4910b1fc3e042a6
Geoff Greer <geoff@greer.fm>
2013-05-25 20:54:55 -0700

    Fix logging of all unicode stuff. This fixes #13

--- plugin/floo/msg.py
+++ plugin/floo/msg.py
@@ -53,11 +53,14 @@ class MSG(object):
         fd.close()
 
     def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __unicode__(self):
         if self.username:
             msg = '[{time}] <{user}> {msg}\n'
         else:
             msg = '[{time}] {msg}\n'
-        return msg.format(user=self.username, time=time.ctime(self.timestamp), msg=self.msg)
+        return unicode(msg).format(user=self.username, time=time.ctime(self.timestamp), msg=self.msg)
 
 
 def msg_format(message, *args, **kwargs):
