aaae841928a486c7347b6f9c02c72189c5114081
Bjorn Tipling <bjorn@ambientchill.com>
2013-12-03 05:49:48 -0800

    Fix vim stomping follow mode issue.

--- plugin/floo/view.py
+++ plugin/floo/view.py
@@ -95,7 +95,10 @@ class View(object):
         self.set_cursor_position(cursor_offset)
 
     def focus(self):
-        vim.command(':silent! edit! %s | :silent! :filetype detect' % self.vim_buf.name)
+        current_buffer = vim.eval('expand("%:p")')
+        if current_buffer != self.vim_buf.name:
+            msg.log("visible buffer changed.")
+            vim.command(':silent! edit! %s | :silent! :filetype detect' % self.vim_buf.name)
 
     def set_cursor_position(self, offset):
         line_num, col = self._offset_to_vim(offset)
