f2b52a803a6c9d8bdb0b7c85d4fc13b59f40d42e
Geoff Greer <geoff@greer.fm>
2013-12-10 09:54:19 -0800

    Squashed 'plugin/floo/common/' changes from 1ae9925..bbeaa11
    bbeaa11 Minor cleanup and pyflakes/pep8
    160ad1a -n Merge commit '4341e3ef02c714e3989b8a8f77a6e4fde662be2c'
    1bbd695 -n Moving has browser into utils.
    2413b5f -n Hadn't saved file. :(
    a00e372 -n Last of the debug statements removed.
    ab03606 -n Reuse editor's file open. More debug statements.
    c737ac1 -n Clean up logging, fix python error.
    afc021b -n Remove debug statement.
    442b110 -n Added link editor.
    398e453 -n Progress on account creation.
    
    git-subtree-dir: plugin/floo/common
    git-subtree-split: bbeaa11ce366e7ae66f06d5efb8f8e75be610c01

--- protocols/floo_proto.py
+++ protocols/floo_proto.py
@@ -76,8 +76,8 @@ class FlooProtocol(base.BaseProtocol):
                 continue
             name = data.get('name')
             try:
-                self.emit("data", name, data)
                 msg.debug("got data " + name)
+                self.emit("data", name, data)
             except Exception as e:
                 print(traceback.format_exc())
                 msg.error('Error handling %s event (%s).' % (name, str(e)))
@@ -232,7 +232,7 @@ class FlooProtocol(base.BaseProtocol):
             return self.reconnect()
 
     def error(self):
-        raise NotImplementedError()
+        raise NotImplementedError("error not implemented.")
 
     def stop(self):
         self.retries = -1
